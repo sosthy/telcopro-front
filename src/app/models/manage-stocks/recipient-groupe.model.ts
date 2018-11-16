9924b85a96964095d2d3d905f12b6c4587ab3127
tinkam <44226021+tinkam@users.noreply.github.com>
2018-11-16 17:29:00 +0100

    correction fournisseurs

--- src/app/models/manage-stocks/recipient-groupe.model.ts
+++ src/app/models/manage-stocks/recipient-groupe.model.ts
@@ -2,16 +2,8 @@ export class RecipientGroupe {
    id: number;
   name: string;
   constructor(obj?: any) {
-    this.id = obj ? (obj.id ? obj.id : null ) : null;
+    this.id = obj ? (obj.id ? obj.id : null) : null;
     this.name = obj ? (obj.name ? obj.name : '') : '';
-import {Recipient} from './recipient.model';
-
-export class RecipientGroupe {
-  id: number;
-  name: string;
-
-  constructor(obj?: any) {
-    this.id = obj ? (obj.id ? obj.connection : null) : null;
-    this.name = obj ? (obj.name ? obj.name : null) : null;
   }
+
 }
--- src/app/models/manage-stocks/recipient.model.ts
+++ src/app/models/manage-stocks/recipient.model.ts
@@ -1,12 +1,5 @@
 import {Mouvment} from './mouvment.model';
 import {RecipientGroupe} from './recipient-groupe.model';
-import {Employee} from '../employee.model';
-import {date} from 'ng2-validation/dist/date';
-
-export class  Recipient {
-  id: number;
-  name: string;
-  surname: string;
 
 export class Recipient {
   id: number;
@@ -24,8 +17,9 @@ export class Recipient {
   location: string;
   groupe: RecipientGroupe;
   mouvements: Mouvment;
+
   constructor(obj?: any) {
-    this.id = obj ? (obj.id ? obj.id : null ) : null;
+    this.id = obj ? (obj.id ? obj.id : null) : null;
     this.name = obj ? (obj.name ? obj.name : '') : '';
     this.surname = obj ? (obj.surname ? obj.surname : '') : '';
     this.website = obj ? (obj.website ? obj.website : '') : '';
@@ -40,26 +34,5 @@ export class Recipient {
     this.location = obj ? (obj.location ? obj.location : '') : '';
     this.groupe = obj ? (obj.groupe ? obj.groupe : new RecipientGroupe()) : new RecipientGroupe();
     this.mouvements = obj ? (obj.id ? obj.id : new Mouvment()) : new Mouvment();
-  enterprise: string;
-  service: string;
-  fonction: string;
-  groupe: RecipientGroupe;
-
-  constructor(obj?: any) {
-    this.id = obj ? (obj.id ? obj.connection : null) : null;
-    this.name = obj ? (obj.name ? obj.name : null) : null;
-    this.birthday = obj ? (obj.birthday ? obj.birthday : new Date()) : new Date();
-    this.surname = obj ? (obj.surname ? obj.surname : null) : null;
-    this.portable = obj ? (obj.portable ? obj.portable : null) : null;
-    this.website = obj ? (obj.website ? obj.website : null) : null;
-    this.sex = obj ? (obj.sex ? obj.sex : null) : null;
-    this.cni = obj ? (obj.cni ? obj.cni : null) : null;
-    this.phone = obj ? (obj.phone ? obj.phone : null) : null;
-    this.photo = obj ? (obj.photo ? obj.photo : null) : null;
-    this.civility = obj ? (obj.civility ? obj.civility : null) : null;
-    this.enterprise = obj ? (obj.enterprise ? obj.enterprise : null) : null;
-    this.groupe = obj ? (obj.groupe ? obj.groupe : null) : null;
-    this.fonction = obj ? (obj.fonction ? obj.fonction : null) : null;
-    this.service = obj ? (obj.service ? obj.service : null) : null;
   }
 }
