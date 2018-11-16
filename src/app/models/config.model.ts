97041f1aeec289433d5b077c47425188ceb7a612
tcheche <eric.njinang@gmail.com>
2018-11-16 02:19:39 -0700

    Update config.model.ts

--- src/app/models/config.model.ts
+++ src/app/models/config.model.ts
@@ -1,11 +1,3 @@
 // export const TELCOPRO_URL = 'http://' + window.location.hostname + ':8080' + '/telcopro';
-// export const TELCOPRO_URL = 'http://' + window.location.hostname + ':8080';
-<<<<<<< HEAD
-// export const TELCOPRO_URL = 'http://' + 'localhost' + ':8080' + '/telcopro';
 // export const TELCOPRO_URL = 'http://' + '192.168.100.200' + ':8080' + '/telcopro';
 export const TELCOPRO_URL = 'http://' + '192.168.100.54' + ':8080' + '/telcopro';
-
-=======
-// export const TELCOPRO_URL = 'http://' + '192.168.100.200' + ':8080' + '/telcopro';
- export const TELCOPRO_URL = 'http://' + '192.168.100.54' + ':8080' + '/telcopro';
->>>>>>> ce199d6d7bd18aabbdb910c0be92c04a99722fa7
