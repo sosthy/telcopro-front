192b505eea0420743fb2248f0c74758ba3b5195d
Sosthene <sosthenegolden@gmail.com>
2018-11-17 23:59:08 +0100

    upd

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -1,31 +1,19 @@
 import { WorkSpace } from './workSpace.model';
+import {Person} from "./person.model";
 
-export class Employee {
-  id: number;
-  name: string;
-  surname: string;
-  phone: string;
-  website: string;
-  sex: string;
-  cni: string;
-  photo: string;
-  birthday: Date;
+export class Employee extends Person {
+
+  poste: string;
   hiringDate: Date;
   seniority: number;
   workSpace: WorkSpace;
 
   constructor(obj?: any) {
-    this.id = obj ? (obj.id ? obj.id : null) : null;
-    this.name = obj ? (obj.name ? obj.name : '') : '';
-    this.surname = obj ? (obj.surname ? obj.surname : '') : '';
-    this.phone = obj ? (obj.phone ? obj.phone : '') : '';
-    this.website = obj ? (obj.website ? obj.website : '') : '';
-    this.sex = obj ? (obj.sex ? obj.sex : '') : '';
-    this.cni = obj ? (obj.cni ? obj.cni : '') : '';
-    this.photo = obj ? (obj.id ? obj.id : '') : '';
-    this.birthday = obj ? (obj.id ? obj.id : new Date()) : new Date();
+    super(obj);
+    this.poste = obj ? (obj.poste ? obj.poste : null) : null;
+    this.hiringDate = obj ? (obj.hiringDate ? obj.hiringDate : null) : null;
     this.seniority = obj ? (obj.seniority ? obj.seniority : null) : null;
-    this.workSpace = obj ? (obj.workSpace ? obj.workSpace : new WorkSpace()) : new WorkSpace();
+    this.workSpace = obj ? (obj.workSpace ? obj.workSpace : null) : null;
   }
 
 }
--- src/app/rh/employees/employees.component.ts
+++ src/app/rh/employees/employees.component.ts
@@ -9,61 +9,70 @@ import {Employee} from '../../models/employee.model';
 })
 
 export class EmployeesComponent implements OnInit {
-  listEmployees = [];
+  listEmployees: Array<Employee> = [];
   keyWords = '';
   pageToLoad = 'listing page'; // listing page, add page, edit page, details page, confirm edit page, confirm add page
   tableMessage = 'Loading.... Please wait!';
   employee: any;
-  workSpaces = [ ];
+  workSpaces = [];
   photoFile: File;
-  constructor(public employeeService: EmployeeService) { }
+
+  constructor(public employeeService: EmployeeService) {
+  }
+
   ngOnInit() {
     this.loadEmployees();
   }
+
   loadEmployees() {
     this.employeeService.getAllEmployees()
       .subscribe(data => {
-        this.listEmployees = data.json();
-        console.log(this.listEmployees);
-        this.tableMessage = 'No employee found';
-      },
+          this.listEmployees = data.json();
+          console.log(this.listEmployees);
+          this.tableMessage = 'No employee found';
+        },
         err => {
-        console.log(err);
+          console.log(err);
         }
-        );
+      );
     this.employeeService.getWorkSpaces()
-    .subscribe(data => {
-        this.workSpaces = data.json();
-        console.log(this.workSpaces);
-      },
+      .subscribe(data => {
+          this.workSpaces = data.json();
+          console.log(this.workSpaces);
+        },
         err => {
-        console.log(err);
+          console.log(err);
         }
-        );
+      );
   }
+
   search() {
     this.employeeService.search(this.keyWords)
       .subscribe(data => {
-        this.listEmployees = data.json();
-        this.tableMessage = 'No results matching';
-      },
+          this.listEmployees = data.json();
+          this.tableMessage = 'No results matching';
+        },
         err => {
-        console.log(err);
+          console.log(err);
         }
-        );
+      );
   }
+
   loadAddPage() {
     this.employee = new Employee();
     this.pageToLoad = 'add page';
   }
+
   submitForm() {
     this.pageToLoad = 'confirm ' + this.pageToLoad;
     console.log(this.photoFile);
   }
+
   loadEditionPage(employee) {
     this.employee = employee;
     this.pageToLoad = 'edit page';
   }
+
   loadPreviewPageConfirmation() {
     if (this.pageToLoad === 'confirm edit page') {
       this.pageToLoad = 'edit page';
@@ -71,46 +80,49 @@ export class EmployeesComponent implements OnInit {
       this.pageToLoad = 'add page';
     }
   }
+
   deleteEmployeeRequest(employee) {
     const confirm = window.confirm('Employee ' + employee.name + ' ' + employee.surname + ' will be detete.');
     if (confirm === true) {
-      this.listEmployees.splice( this.listEmployees.indexOf(employee), 1);
+      this.listEmployees.splice(this.listEmployees.indexOf(employee), 1);
       this.employeeService.deleteEmployee(employee.id)
         .subscribe(data => {
-          alert('Employee ' + employee.name + ' ' + employee.surname + ' has been succeful removed');
-        },
+            alert('Employee ' + employee.name + ' ' + employee.surname + ' has been succeful removed');
+          },
           err => {
-          alert('problem');
+            alert('problem');
           });
     }
   }
+
   loadDetailPage(employee) {
     this.employee = employee;
     this.pageToLoad = 'details page';
   }
+
   saveInformation() {
     const page = this.pageToLoad;
-     /*
-     this.employeeService.saveEmployee(this.employee, this.photoFile)
-     .subscribe(data => {
-        this.employee = data.json();
-      },
+    /*
+    this.employeeService.saveEmployee(this.employee, this.photoFile)
+    .subscribe(data => {
+       this.employee = data.json();
+     },
+       err => {
+       console.log(err);
+       });
+     */
+    this.employeeService.saveEmployee(this.employee)
+      .subscribe(data => {
+          this.employee = data.json();
+          if (page === 'confirm add page') {
+            this.listEmployees.push(this.employee);
+            console.log('Result Save information', this.employee);
+          }
+        },
         err => {
-        console.log(err);
+          console.log(err);
         });
-      */
-     this.employeeService.saveEmployee(this.employee)
-     .subscribe(data => {
-        this.employee = data.json();
-        if (page === 'confirm add page') {
-          this.listEmployees.push(this.employee);
-          console.log('Result Save information', this.employee);
-        }
-      },
-        err => {
-        console.log(err);
-        });
-      alert('Employee ' + this.employee.name + ' ' + this.employee.surname + ' has been succeful done.');
-      this.pageToLoad = 'listing page';
+    alert('Employee ' + this.employee.name + ' ' + this.employee.surname + ' has been succeful done.');
+    this.pageToLoad = 'listing page';
   }
 }
